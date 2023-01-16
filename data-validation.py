92f8ca7214654c57617199a613ea45b403458a0b
Fikri Muza <fikrimuzadi@gmail.com>
2023-01-16 21:55:08 +0700

    excercise update

--- data-validation.py
+++ data-validation.py
@@ -1,26 +1,5 @@
-"""
-
-"""
-data_valid = False
-
-while data_valid == False:
-    grade1 = float(input("type the grade of the first test: "))
-    if grade1 < 0 and grade1 > 10:
-        print("Grade should be between 0 and 10")
-        continue
-    else:
-        data_valid = True
-
-data_valid = False
-
-while data_valid == False:
-    grade2 = float(input("type the grade of the second test: "))
-    if grade2 < 0 and grade2 > 10:
-        print("Grade should be between 0 and 10")
-        continue
-    else:
-        data_valid = True
-
+grade1 = float(input("type the grade of the first test: "))
+grade2 = float(input("type the grade of the second test: "))
 absences = int(input("Type the total number of classes: "))
 total_classes = int(input("Type the total number of classes: "))
 
@@ -28,7 +7,7 @@ avg_grade = (grade1 + grade2) / 2
 attendance = (total_classes - absences) / total_classes
 
 print("Average grade: ", round(avg_grade, 2))
-print("Attendance rate: ", str( round((attendance * 100), 2)) + "%")
+print("Attendance rate: ", str(round((attendance * 100), 2)) + "%")
 
 if 6 <= avg_grade >= 0.8:
     print("The student has apporved.")
@@ -38,5 +17,3 @@ elif attendance >= 0.8:
     print("The student has failed due to an average grade lower than 6.0.")
 else:
     print("The student has failed due to an average grade lower than 80%.")
-
-
