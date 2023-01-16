5089c80be6c2f4c620a00c93093d67d3a9bae278
Fikri Muza <fikrimuzadi@gmail.com>
2023-01-16 22:51:59 +0700

    basic excercise

--- error-handling.py
+++ error-handling.py
@@ -1,6 +1,15 @@
+"""
+Apply the try and except statements to the student grades program and keep it from crashing.
+"""
 data_valid = False
 while not data_valid:
-    grade1 = float(input("type the grade of the first test: "))
+    grade1 = input("type the grade of the first test: ")
+    try:
+        grade1 = float(grade1)
+    except:
+        print("Invalid input. Only numbers are accepted. Decimmals should be separated with a dot.")
+        continue
+
     if grade1 < 0 or grade1 > 10:
         print("Grade should be between 0 and 10")
     else:
@@ -8,7 +17,13 @@ while not data_valid:
 
 data_valid = False
 while not data_valid:
-    grade2 = float(input("type the grade of the second test: "))
+    grade2 = input("type the grade of the second test: ")
+    try:
+        grade2 = float(grade2)
+    except:
+        print("Invalid input. Only numbers are accepted. Decimmals should be separated with a dot.")
+        continue
+
     if grade2 < 0 or grade2 > 10:
         print("Grade should be between 0 and 10")
     else:
@@ -16,7 +31,13 @@ while not data_valid:
 
 data_valid = False
 while not data_valid:
-    total_classes = int(input("Type the total number of classes: "))
+    total_classes = input("Type the total number of classes: ")
+    try:
+        total_classes = int(total_classes)
+    except:
+        print("Invalid input. Only numbers are accepted. Decimmals should be separated with a dot.")
+        continue
+
     if total_classes <= 0:
         print("The number of classes can't zero or less")
     else:
@@ -24,7 +45,13 @@ while not data_valid:
 
 data_valid = False
 while not data_valid:
-    absences = int(input("Type the total number of absences: "))
+    absences = input("Type the total number of absences: ")
+    try:
+        absences = int(absences)
+    except:
+        print("Invalid input. Only numbers are accepted. Decimmals should be separated with a dot.")
+        continue
+
     if absences < 0 or absences > total_classes:
         print("The number of absences can't zero or greater than the number of total classes.")
     else:
